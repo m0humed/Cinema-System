6b23e8f4dcabc77f4bf9924f086669b722268731
m0humed <qandilmohammed83@gmail.com>
2025-12-25 15:54:04 +0200

    Make Token Create Direct after create user

--- Cenima/cenimaSystem/tickets/models.py
+++ Cenima/cenimaSystem/tickets/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+from django.db.models.signals import  post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 # Create your models here.
 
 # -- Movies  -- Guest -- Reservation
@@ -8,9 +13,6 @@ class Movie(models.Model):
     hall = models.CharField(max_length=10)
     name = models.CharField(max_length=20)
     date = models.DateField()
-    
-    
-
 
 class Guest(models.Model):
     name = models.CharField(max_length=20 , null=False)  
@@ -21,4 +23,7 @@ class Reservation(models.Model):
     guest = models.ForeignKey(Guest ,on_delete=models.CASCADE,related_name="Reservation")
 
 
-
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)
+def createToken(sender , instance , created , **kwargs):
+    if created:
+        Token.objects.create(user=instance)
